fb3e6e30ba7056e76a00f45fc0a1db97defea152
Yoon <ho8278@naver.com>
2019-01-29 18:02:12 +0900

    issue-007-TAB1-Main레이아웃 추가

--- app/src/main/java/com/teame/boostcamp/myapplication/ui/ChekListCreateFragment.java
+++ app/src/main/java/com/teame/boostcamp/myapplication/ui/ChekListCreateFragment.java
@@ -1,4 +1,106 @@
 package com.teame.boostcamp.myapplication.ui;
 
+<<<<<<< Updated upstream
 public class ChekListCreateFragment {
+=======
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.teame.boostcamp.myapplication.R;
+
+public class ChekListCreateFragment extends Fragment implements OnMapReadyCallback {
+
+    MapView map=null;
+    LinearLayout ll=null;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_tab1_main, container, false);
+        TextInputEditText text=(TextInputEditText)view.findViewById(R.id.searchText);
+        text.setOnFocusChangeListener((v, hasFocus) -> {
+            if(hasFocus){
+                ll.setVisibility(View.VISIBLE);
+            }
+        });
+        map=(MapView)view.findViewById(R.id.map);
+        map.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        map.onLowMemory();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        map.onDestroy();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        map.onStart();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        map.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        map.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+    }
+>>>>>>> Stashed changes
 }
--- app/src/main/java/com/teame/boostcamp/myapplication/ui/MainActivity.java
+++ app/src/main/java/com/teame/boostcamp/myapplication/ui/MainActivity.java
@@ -2,14 +2,19 @@ package com.teame.boostcamp.myapplication.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+
 import com.teame.boostcamp.myapplication.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment,new ChekListCreateFragment())
+                .commit();
     }
 }
