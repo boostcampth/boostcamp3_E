97ad4abe3c0b845c022307917a6e7c24f32891c0
Hongbeom <idd7403@naver.com>
2019-01-30 10:30:51 +0900

    issue-006-회원가입 기능 구현

---
+++ app/src/main/java/com/teame/boostcamp/myapplication/authentication/SignUpContractor.java
@@ -0,0 +1,16 @@
+package com.teame.boostcamp.myapplication.authentication;
+
+import android.app.Activity;
+
+public interface SignUpContractor {
+
+    interface View{
+        void makeToast(String message);
+        void startNextActivity();       // 로그인 후 나오는 메인페이지 이동 액티비티
+    }
+
+    interface Presenter{
+        void onEmailCheckButtonClicked(String email);
+        void onSignUpButtonClicked(Activity activity, String email, String password, User userData);
+    }
+}
---
+++ app/src/main/java/com/teame/boostcamp/myapplication/authentication/User.java
@@ -0,0 +1,33 @@
+package com.teame.boostcamp.myapplication.authentication;
+
+public class User{
+
+    private String userEmail;
+    private String userPassword;
+    private int userAge;
+    private String userSex;
+
+    public User(){}
+
+    public User(String userEmail, String userPassword, int userAge, String userSex){
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userAge = userAge;
+        this.userSex = userSex;
+    }
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public String getUserSex() {
+        return userSex;
+    }
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+}
