7c188895364ffcfa7f0085693151b32aefbc73d3
godueol <weogjw01@naver.com>
2019-01-31 18:43:27 +0900

    issue-026-fix-init-item-detail-activity

--- app/src/main/java/com/teame/boostcamp/myapplication/ui/itemdetail/ItemDetailActivity.java
+++ app/src/main/java/com/teame/boostcamp/myapplication/ui/itemdetail/ItemDetailActivity.java
@@ -1,11 +1,14 @@
 package com.teame.boostcamp.myapplication.ui.itemdetail;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 
 import com.teame.boostcamp.myapplication.R;
 import com.teame.boostcamp.myapplication.databinding.ActivityItemDetailBinding;
 import com.teame.boostcamp.myapplication.ui.base.BaseMVPActivity;
+import com.teame.boostcamp.myapplication.ui.createlist.CreateListActivity;
 
 public class ItemDetailActivity extends BaseMVPActivity<ActivityItemDetailBinding,ItemDetailContract.Presenter> implements ItemDetailContract.View {
 
@@ -27,7 +30,10 @@ public class ItemDetailActivity extends BaseMVPActivity<ActivityItemDetailBindin
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_detail);
     }
 
+    public static void startActivity(Context context) {
+        Intent intent = new Intent(context, ItemDetailActivity.class);
+        context.startActivity(intent);
+    }
 }
