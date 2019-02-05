07757892b04344ca0731cfdd0fb667b8f401ad6c
Hongbeom <idd7403@naver.com>
2019-02-05 14:43:53 +0900

    issue-028-글 작성 뷰와 프레젠터

--- app/src/main/java/com/teame/boostcamp/myapplication/ui/addpost/AddPostPresenter.java
+++ app/src/main/java/com/teame/boostcamp/myapplication/ui/addpost/AddPostPresenter.java
@@ -1,12 +1,59 @@
 package com.teame.boostcamp.myapplication.ui.addpost;
 
-import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.teame.boostcamp.myapplication.model.entitiy.Post;
+import com.teame.boostcamp.myapplication.util.LocalImageUtil;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
 
 public class AddPostPresenter implements AddPostContract.Presenter {
     private AddPostContract.View view;
+    private StorageReference mStorageRef;
+    private FirebaseFirestore db;
+    private FirebaseAuth auth;
 
     public AddPostPresenter(AddPostContract.View view) {
         this.view = view;
+    }
+
+
+    @Override
+    public void addPost(String title, String content, List<Uri> uriList) {
+        auth = FirebaseAuth.getInstance();
+        ArrayList<String> storagePathList = new ArrayList<>();
+        for(int i=0; i<uriList.size(); i++){
+            Uri file = Uri.fromFile(new File(LocalImageUtil.getPath( (Context) view, uriList.get(i))));
+            mStorageRef = FirebaseStorage.getInstance().getReference().child("images/post/"+auth.getUid() + "/" + title + "/" + i + ".jpg");
+            storagePathList.add("images/post/"+auth.getUid() + "/" + title + "/" + i + ".jpg");
+            mStorageRef.putFile(file);
+        }
+        Post post = new Post(title, content, storagePathList);
+        db = FirebaseFirestore.getInstance();
+        db.collection("posts").add(post).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                view.succeedAddPost();
+            }
+        });
+
+    }
+
+    @Override
+    public void takePicture() {
+
     }
 
     @Override
@@ -17,21 +64,6 @@ public class AddPostPresenter implements AddPostContract.Presenter {
     @Override
     public void onDetach() {
         this.view=null;
-    }
-
-    @Override
-    public void addPost() {
-
-    }
-
-    @Override
-    public void pickGalleryImages() {
-
-    }
-
-    @Override
-    public void takePicture() {
-
     }
 }
 
