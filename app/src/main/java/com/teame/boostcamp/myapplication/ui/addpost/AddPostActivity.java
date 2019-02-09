c3a30daa0f6391bcec82333359c087e75d599b4d
Hongbeom <idd7403@naver.com>
2019-02-09 16:16:48 +0900

    issue-064:dispose조건수정

--- app/src/main/java/com/teame/boostcamp/myapplication/ui/addpost/AddPostActivity.java
+++ app/src/main/java/com/teame/boostcamp/myapplication/ui/addpost/AddPostActivity.java
@@ -107,7 +107,7 @@ public class AddPostActivity extends BaseMVPActivity<ActivityAddPostBinding, Add
     protected void onDestroy() {
         super.onDestroy();
         presenter.onDetach();
-        if(!disposable.isDisposed()){
+        if(disposable != null){
             disposable.dispose();
         }
     }
