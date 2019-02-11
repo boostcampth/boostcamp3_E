a77fb13e7a288b8f8a22cd8369a5882acea83298
Hongbeom <idd7403@naver.com>
2019-02-11 17:52:48 +0900

    issue-074-댓글 기능 구현

--- app/src/main/java/com/teame/boostcamp/myapplication/model/repository/remote/PostListRemoteDataSource.java
+++ app/src/main/java/com/teame/boostcamp/myapplication/model/repository/remote/PostListRemoteDataSource.java
@@ -7,9 +7,11 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.teame.boostcamp.myapplication.model.entitiy.Post;
+import com.teame.boostcamp.myapplication.model.entitiy.Reply;
 import com.teame.boostcamp.myapplication.model.repository.PostListDataSource;
 import com.teame.boostcamp.myapplication.util.DLogUtil;
 
+import java.util.Date;
 import java.util.List;
 
 import io.reactivex.Single;
@@ -18,7 +20,7 @@ import io.reactivex.subjects.PublishSubject;
 public class PostListRemoteDataSource implements PostListDataSource {
 
     private static final String QUERY_POST = "posts";
-
+    private static final String QUERY_REPLY = "replies";
 
     private static PostListRemoteDataSource INSTANCE;
 
@@ -75,6 +77,53 @@ public class PostListRemoteDataSource implements PostListDataSource {
 
         // 모든 포스트 List 반환
         return subject.toList();
+    }
+
+    @Override
+    public Single<List<Reply>> loadPostReplyList(String postUid) {
+
+        PublishSubject<Reply> subject = PublishSubject.create();
+
+        Task reply = baseRef.document(postUid)
+                .collection(QUERY_REPLY)
+                .get()
+                .addOnSuccessListener(documents -> {
+
+                    for (DocumentSnapshot document : documents) {
+                        Reply postReply = document.toObject(Reply.class);
+                        DLogUtil.d(postReply.toString());
+                        subject.onNext(postReply);
+                    }
+
+                });
+
+        Tasks.whenAll(reply).addOnSuccessListener(aVoid -> subject.onComplete())
+                .addOnFailureListener(e -> subject.onError(new Throwable(e.getMessage())));
+
+        return subject.toList();
+    }
+    @Override
+    public Single<Reply> writePostReply(String postUid, String content) {
+        Reply reply = new Reply();
+        reply.setContent(content);
+        reply.setWriter(auth.getCurrentUser().getEmail());
+        long now = System.currentTimeMillis();
+        Date nowDate = new Date(now);
+        reply.setWriteDate(nowDate);
+        PublishSubject<Reply> subject = PublishSubject.create();
+        Task saveReply = baseRef.document(postUid)
+                .collection(QUERY_REPLY)
+                .add(reply)
+                .addOnSuccessListener(aVoid -> {
+                    reply.setKey(postUid);
+                    subject.onNext(reply);
+                })
+                .addOnFailureListener(subject::onError);
+        Tasks.whenAll(saveReply).addOnCompleteListener(task -> {
+            subject.onComplete();
+        });
+
+        return subject.flatMapSingle(Single::just).single(reply);
     }
 }
 
